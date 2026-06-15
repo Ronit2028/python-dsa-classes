abb23db4fdfba64b224d073883357f7d8c93d1cf
Ronit2028 <Ronit@2525>
2026-06-15 12:28:30 +0530

    class 15 june

---
+++ JUNE_15_CLASS/class.py
@@ -0,0 +1,36 @@
+# Understanding time complexity
+
+# how execution time grows -> input size
+# arr = [1,2,6,4,5,3]
+# string = "Hello World" 
+# Input size:
+#               (n) -> number of elements in the input
+# For arrays -> n
+# for strings -> n
+# for graphs -> V and E
+
+# O(1)
+# O(6)
+# O(n)
+
+# two dimensional array
+# arr2 = [[1,2,3],[4,5,6],[7,8,9]]
+# arr2 = [[1,2],[3,4]]
+# for i in range(len(arr2)): * 3
+#     for j in range(len(arr2[i])): *3
+#         print(arr2[i][j])
+
+# O(3X3)
+
+
+# while (n>1):
+#     n = n/2
+
+# O(logn)
+
+# 2n + 5 -> O(n)
+# 2n
+# n -> O(n)
+
+# 5n2 + n + 2
+
